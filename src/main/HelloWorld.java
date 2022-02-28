b8ff40064cae94e86d312bf41bfb318d2d37a2ce
Micah Wood <micahswood7@gmail.com>
2022-02-28 12:16:05 -0600

    finished exercise

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -2,6 +2,9 @@ package main;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+        System.out.print("Hello");
+        System.out.print(" World!");
+        System.out.print("\n");
     }
 }
