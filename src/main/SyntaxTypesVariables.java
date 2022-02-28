7fc270a906ada9a3ba705df515bc040a8ad10462
Micah Wood <micahswood7@gmail.com>
2022-02-28 15:36:06 -0600

    finishing Syntax, Types, and Variables exercise

---
+++ src/main/SyntaxTypesVariables.java
@@ -0,0 +1,58 @@
+package main;
+
+public class SyntaxTypesVariables {
+    public static void main(String[] args){
+        int myFavoriteNumber = 18;
+        System.out.print(myFavoriteNumber);
+        System.out.print("\n");
+
+        String myString = "3.14159";
+        System.out.print(myString);
+        System.out.print("\n");
+
+        float myNumber = (float) 3.14;
+        System.out.print(myNumber);
+        System.out.print("\n");
+
+        /* int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+        System.out.print("\n"); **/
+
+        /* int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+        System.out.print("\n"); **/
+
+        //char class;
+
+        /* String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+        System.out.print(three); **/
+
+        /* int three = (int) "three";
+        System.out.print(three);
+        System.out.print("\n"); **/
+
+         /* int x = 4;
+        x += 5;
+        System.out.print(x); **/
+
+
+        /* int x = 3;
+        int y = 4;
+        y *= x;
+        System.out.print(y); **/
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.print(x);
+        System.out.print("\n");
+        System.out.print(y);
+
+    }
+
+}
