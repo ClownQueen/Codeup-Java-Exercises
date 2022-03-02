2c75458d5cdef44cda25d012a090e30f3eebb9c1
Micah Wood <micahswood7@gmail.com>
2022-03-02 11:01:26 -0600

    working on ControlFlowExercises.java

---
+++ src/main/ControlFlowExercises.java
@@ -0,0 +1,51 @@
+package main;
+
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println("i is: " + i);
+//            i++;
+//        }
+
+//        int j = 100;
+//        do {
+//            System.out.println(j);
+//            j -= 5;
+//        } while (j >= -(10));
+
+//        long a = 2;
+//        do {
+//            System.out.println(a);
+//            a *= a;
+//        }while (a < 1000000);
+
+//        for (int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+
+//        for (long i = 2; i < 1000000; i *= i) {
+//            System.out.println(i);
+//        }
+
+//        for (int i = 1; i < 101; i++) {
+//            if (i % 15 == 0){
+//                System.out.print("FIZZ BUZZ\n");
+//            } else if (i % 3 == 0){
+//                System.out.print("FIZZ\n");
+//            } else if (i % 5 == 0){
+//                System.out.print("BUZZ\n");
+//            } else {
+//                System.out.print(i + "\n");
+//            }
+//        }
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Where would you like the table to end?: ");
+        int myInput = input.nextInt();
+
+    }
+}
