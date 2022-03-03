ae98795085667fa2a5633f9047a9a74eab0c6ebe
Micah Wood <micahswood7@gmail.com>
2022-03-03 10:42:30 -0600

    edited StringExercise.java

--- src/main/StringExercise.java
+++ src/main/StringExercise.java
@@ -2,20 +2,17 @@ package main;
 
 public class StringExercise {
     public static void main(String[] args) {
-        String str1 = "We don't need no education";
+        String str1 = "We don't need no education\n We don't need no thought control";
         System.out.println(str1 + '\n');
 
-        String str2 = "We don't need no thought control";
-        System.out.println(str2 + '\n');
+        String str2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(str2 + "\n");
 
-        String str3 = "Check \"this\" out!, \"s inside of \"s!";
-        System.out.println(str3 + "\n");
+        String str3 = "In windows, the main drive is usually C:\\";
+        System.out.println(str3 + '\n');
 
-        String str4 = "In windows, the main drive is usually C:\\";
-        System.out.println(str4 + '\n');
-
-        String str5 = "I can do backslashes \\, double backslashes \\\\,\n" +
+        String str4 = "I can do backslashes \\, double backslashes \\\\,\n" +
                 "and the amazing triple backslash \\\\\\!";
-        System.out.println(str5);
+        System.out.println(str4);
     }
 }
